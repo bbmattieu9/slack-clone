228e8e03193c66fd4ce46f868505b19f80923a8f
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-25 01:42:10 +0100

    [Header] add header structure && material ui core && material ui icons

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,21 @@ import './Header.css'
 
 function Header() {
     return (
-        <div>
-            
+        <div className='header'>
+
+            <div className="header__left">
+                {/* avatar for loggin user */}
+                {/* Time Icon */}
+            </div>
+
+            <div className="header__search">
+                {/* Search Icon */}
+                {/*  input */}
+            </div>
+
+            <div className="header_right">
+                {/* help Icon */}
+            </div>
         </div>
     )
 }
