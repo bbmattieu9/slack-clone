6c5bd436fd7661968088c1e74e801ec8279a62b8
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-26 02:11:51 +0100

    [Header] add header content

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Header.css'
+import { Avatar } from '@material-ui/core';
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 function Header() {
     return (
@@ -7,16 +11,21 @@ function Header() {
 
             <div className="header__left">
                 {/* avatar for loggin user */}
-                {/* Time Icon */}
+                <Avatar 
+                    className='header__avatar'
+                    alt='tizi Cerutti'
+                    src=''
+                />
+                <AccessTimeIcon />
             </div>
 
             <div className="header__search">
-                {/* Search Icon */}
-                {/*  input */}
+                <SearchIcon />
+                <input placeholder=' e.g. search for developer tizi cerutti' />
             </div>
 
             <div className="header_right">
-                {/* help Icon */}
+                <HelpOutlineIcon />
             </div>
         </div>
     )
