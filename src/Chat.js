d91433ec1c14b51f30d0f0f414392d14933b11d2
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-31 00:19:31 +0100

    [Chat] add chat content

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import './Chat.css'
 import { useParams } from 'react-router-dom'
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 
 function Chat() {
-    return <div className='chat'>
-        
-    </div>;
+
+    const { roomId } = useParams();
+    return ( 
+    <div className='chat'>
+        <h2>you are in the { roomId } room</h2>
+
+        <div className="chat__header">
+
+            <div className="chat__headerLeft">
+                <h4 className="chat__channelName">
+                    <strong>
+                        #general
+                    </strong>
+                    <StarBorderOutlinedIcon />
+                </h4>
+            </div>
+
+            <div className="chat__headerRight">
+
+            </div>
+        </div>
+    </div>
+    );
 }
 
 export default Chat
