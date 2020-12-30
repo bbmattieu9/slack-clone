c1889436f9e262a2cefc2aa5d4559da0ac87187f
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-30 17:05:34 +0100

    [Routing] add chat component

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+import { useParams } from 'react-router-dom'
+
+function Chat() {
+    return <div className='chat'>
+        
+    </div>;
+}
+
+export default Chat
