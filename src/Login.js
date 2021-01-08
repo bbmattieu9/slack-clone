9cd75bd77e7a68f2ffe678087bc10609ab20600d
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2021-01-08 11:27:54 +0100

    [Auth] add firebase signupwithpopup

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './Login.css';
 import { Button } from '@material-ui/core';
+import { auth, provider } from './firebase';
 
 function Login() {
+
+    const signIn = (e) => {
+        e.preventDefault();
+        auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          
+        //   const credential = result.credential;
+    
+        }).catch((error) => {
+          console.log('[Auth Error]:', error.message);
+        });
+    }
+
     return <div className='login'>
             <div className="login__container">
-
                 <img 
                     src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd-1024-80.jpg.webp" 
                     alt="slack logo img"/>
 
                     <h1>Sign in to fake Slack HQ</h1>
                     <p>babatoondayMattieu.slack.com</p>
-                    <Button>Sign in with Google</Button>
+                    <Button onClick={signIn}>Sign in with Google</Button>
             </div>
         </div>;
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBowUN_e6vXGTDjSA-Ln1EFXpiQvnUD_xk",
-    authDomain: "slack-clone-dd1f6.firebaseapp.com",
-    projectId: "slack-clone-dd1f6",
-    storageBucket: "slack-clone-dd1f6.appspot.com",
-    messagingSenderId: "751793000445",
-    appId: "1:751793000445:web:3be011fcd792a90ad15394",
-    measurementId: "G-R5T00FJ13X"
-  };
+  apiKey: "AIzaSyBowUN_e6vXGTDjSA-Ln1EFXpiQvnUD_xk",
+  authDomain: "slack-clone-dd1f6.firebaseapp.com",
+  projectId: "slack-clone-dd1f6",
+  storageBucket: "slack-clone-dd1f6.appspot.com",
+  messagingSenderId: "751793000445",
+  appId: "1:751793000445:web:3be011fcd792a90ad15394",
+  measurementId: "G-R5T00FJ13X"
+};
 
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
